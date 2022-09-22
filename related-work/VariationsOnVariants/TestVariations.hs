3d9c883b725ba7931ac62fc6ef7007ab011be55b
Anna <romeborn@student.chalmers.se>
2022-09-22 11:49:15 +0200

    Make TestVariations a main module and add example. Also add eval and render functions that handle Neg as well

--- related-work/VariationsOnVariants/TestVariations.hs
+++ related-work/VariationsOnVariants/TestVariations.hs
@@ -1,4 +1,6 @@
-module TestVariations where
+{-# LANGUAGE TypeOperators #-}
+
+module Main where
 
 import VariationsOnVariants
 import ExprVariations
@@ -7,10 +9,47 @@ import EvalVariations
 import RenderVariations
 import DesugVariations
 
+-- | Examples of type Expr, containing constants, addition and multiplication
+addExample :: Expr
+addExample = iAdd (iConst 3) (iConst 5)
 
+addMulExample :: Expr
+addMulExample = iMul (iConst 2) addExample
+                      
+-- | Composed type Expr', also containing negation
+type Expr' = Term (Const :+: Op :+: Neg)
 
-exOp :: Expr
-exOp = inj' (inj' (Const 1) `Add` inj' (Const 2)) 
+-- | Evaluation function also considering negation
+evalWithNeg :: Expr' -> Int
+evalWithNeg = cases (evalNeg ? (evalConst ? evalOp))
 
-exOp2 :: Expr
-exOp2 = inj' (inj' (Const 3) `Mul` exOp)
+-- | Rendering function also considering negation
+renderWithNeg :: Expr' -> String
+renderWithNeg = cases (renderNeg ? (renderConst ? renderOp))
+
+-- | Example with negation
+negAddExample :: Expr'
+negAddExample = iConst 3 `iAdd` (iNeg (iConst 5))
+
+-- | Evaluation examples
+evalAddMul = eval addMulExample
+
+evalNegAdd = evalWithNeg negAddExample
+
+-- | Render example
+renderNegAdd = renderWithNeg negAddExample
+
+-- | Desugar example
+desugNegAdd = render (desug negAddExample :: Expr)
+
+-- | Main, printing results of above examples
+main :: IO ()
+main = do
+    putStrLn "Evaluation examples:"
+    print evalAddMul
+    print evalNegAdd
+    putStrLn "Render example:"
+    putStrLn renderNegAdd
+    putStrLn "Desugar example:"
+    putStrLn desugNegAdd
+
