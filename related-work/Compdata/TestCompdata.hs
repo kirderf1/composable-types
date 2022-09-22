63f227975b4cea203efe0b036fc15dc0fd56f7f7
Anna <romeborn@student.chalmers.se>
2022-09-22 11:38:08 +0200

    Make TestCompdata a main module and add examples

--- related-work/Compdata/TestCompdata.hs
+++ related-work/Compdata/TestCompdata.hs
@@ -1,4 +1,6 @@
-module TestCompdata where
+{-# LANGUAGE TypeOperators #-}
+
+module Main where
 
 import ExprCompdata
 import EvalCompdata
@@ -8,6 +10,38 @@ import DesugCompdata
 
 import Data.Comp 
 
--- Example: evalEx = iConst 5
-evalEx :: Int
-evalEx = eval (iConst 1 `iAdd` (iConst 2 `iMul` iConst 2) :: Expr)
+-- | Examples of type Expr, containing constants, addition and multiplication
+addExample :: Expr
+addExample = iAdd (iConst 3) (iConst 5)
+
+addMulExample :: Expr
+addMulExample = iMul (iConst 2) addExample
+                      
+-- | Composed type Expr', also containing negation
+type Expr' = Term (Value :+: Op :+: Neg)
+
+-- | Example with negation
+negAddExample :: Expr'
+negAddExample = iConst 3 `iAdd` (iNeg (iConst 5))
+
+-- | Evaluation examples
+evalAddMul = eval addMulExample
+
+evalNegAdd = eval negAddExample
+
+-- | Render example
+renderNegAdd = pretty negAddExample
+
+-- | Desugar example
+desugNegAdd = pretty (desug negAddExample :: Expr)
+
+-- | Main, printing results of above examples
+main :: IO ()
+main = do
+    putStrLn "Evaluation examples:"
+    print evalAddMul
+    print evalNegAdd
+    putStrLn "Render example:"
+    putStrLn renderNegAdd
+    putStrLn "Desugar example:"
+    putStrLn desugNegAdd
