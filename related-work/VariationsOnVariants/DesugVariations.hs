8bb0652d08e5a350beb19751ddf9feb5a972d8d5
Anna <romeborn@student.chalmers.se>
2022-09-23 10:08:27 +0200

    More general type signature for desugDef in Variations on Variants example

--- related-work/VariationsOnVariants/DesugVariations.hs
+++ related-work/VariationsOnVariants/DesugVariations.hs
@@ -11,9 +11,7 @@ desugNeg :: (Const :<: f, Op :<: f) => Neg e -> (e -> Term f) -> Term f
 desugNeg (Neg e) r = iMul (iConst (-1)) (r e)
 
 -- | Default case for desugaring where there is nothing to desugar
-desugDef :: (f :-: Neg ~ g, Without f Neg (Minus f Neg), 
-            Op :<: g, Const :<: g, Functor g) =>
-            g (Term f) -> (Term f -> Term g) -> Term g
+desugDef :: Functor g => g (Term f) -> (Term f -> Term g) -> Term g
 desugDef e r = In (fmap r e)
 
 -- | Desugaring function that removes negation and leave the rest as it is
