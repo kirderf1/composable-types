8c729494d1aceb903a6fb9f282bb479168403432
Anna <romeborn@student.chalmers.se>
2022-11-18 17:30:28 +0100

    Use Neg instead of Sug as type constructor name

--- related-work/TreesThatGrow/NegationTrees.hs
+++ related-work/TreesThatGrow/NegationTrees.hs
@@ -7,47 +7,47 @@ import EvalTrees
 import AsStringTrees
 import Data.Void
 
--- | Sugar
+-- | Negar
 
 -- | Type instance for the original Expr type, now containing 
--- Sug as as its extension variant
-type instance X_ExprExt WithNeg = Sug WithNeg
+-- Neg as as its extension variant
+type instance X_ExprExt WithNeg = Neg WithNeg
 
 data WithNeg
 
--- | Data type for Sug, containing Neg for negation, and is also 
+-- | Data type for Neg, containing Neg for negation, and is also 
 -- extensible in variants.
-data Sug e = Neg (Expr e) | SugExt (X_SugExt e)
+data Neg e = Neg (Expr e) | NegExt (X_NegExt e)
 
--- | The extension field of Sug is defined through a type family
-type family X_SugExt e
+-- | The extension field of Neg is defined through a type family
+type family X_NegExt e
 
--- | Type instance for the composition of Sug that is not extended
-type instance X_SugExt WithNeg = Void
+-- | Type instance for the composition of Neg that is not extended
+type instance X_NegExt WithNeg = Void
 
  -- | Useful pattern synonym
 pattern Neg_WithNeg :: Expr WithNeg -> Expr WithNeg
 pattern Neg_WithNeg e <- ExprExt (Neg e)
     where Neg_WithNeg e = ExprExt (Neg e)
           
--- | Evaluation of Sug          
-evalSug :: (X_ExprExt e ~ Sug e) 
-    => (X_SugExt e -> Int) -> Sug e -> Int
-evalSug f (Neg e) = (-1) * eval (evalSug f) e
-evalSug f (SugExt e) = f e
+-- | Evaluation of Neg          
+evalNeg :: (X_ExprExt e ~ Neg e) 
+    => (X_NegExt e -> Int) -> Neg e -> Int
+evalNeg f (Neg e) = (-1) * eval (evalNeg f) e
+evalNeg f (NegExt e) = f e
 
--- | Evaluation of the composition of Expr where it is extended with Sug,
--- where Sug has no extensions
+-- | Evaluation of the composition of Expr where it is extended with Neg,
+-- where Neg has no extensions
 evalWithNeg :: Expr WithNeg -> Int
-evalWithNeg e = eval (evalSug absurd) e
+evalWithNeg e = eval (evalNeg absurd) e
 
--- | asString of Sug
-asStringSug :: (X_ExprExt e ~ Sug e) 
-    => (X_SugExt e -> String) -> Sug e -> String
-asStringSug f (Neg e) = "(-" ++ asString (asStringSug f) e ++ ")"
-asStringSug f (SugExt e) = f e
+-- | asString of Neg
+asStringNeg :: (X_ExprExt e ~ Neg e) 
+    => (X_NegExt e -> String) -> Neg e -> String
+asStringNeg f (Neg e) = "(-" ++ asString (asStringNeg f) e ++ ")"
+asStringNeg f (NegExt e) = f e
 
--- | asString of the composition of Expr where it is extended with Sug,
--- where Sug has no extensions
+-- | asString of the composition of Expr where it is extended with Neg,
+-- where Neg has no extensions
 asStringWithNeg :: Expr WithNeg -> String
-asStringWithNeg e = asString (asStringSug absurd) e
+asStringWithNeg e = asString (asStringNeg absurd) e
