13acfa285a909033b2eeae152a695895e3ab0f37
Anna <romeborn@student.chalmers.se>
2022-11-18 17:35:27 +0100

    A polymorphic desug function in Trees that Grow

--- related-work/TreesThatGrow/DesugTrees.hs
+++ related-work/TreesThatGrow/DesugTrees.hs
@@ -7,7 +7,7 @@ import NegationTrees
 import Data.Void
 
 -- | Desugaring function for Expr
-desug :: (X_ExprExt e -> Expr UD) -> Expr e -> Expr UD
+desug :: (X_ExprExt e1 -> Expr e2) -> Expr e1 -> Expr e2
 desug _ (Const i) = Const i
 desug f (Add e1 e2) = Add (desug f e1) (desug f e2)
 desug f (Mul e1 e2) = Mul (desug f e1) (desug f e2)
@@ -18,8 +18,8 @@ desugUD :: Expr UD -> Expr UD
 desugUD = desug absurd
 
 -- | Desugaring of Neg
-desugNeg :: (X_ExprExt e ~ Neg e) 
-    => (X_NegExt e -> Expr UD) -> Neg e -> Expr UD
+desugNeg :: (X_ExprExt e1 ~ Neg e1) 
+    => (X_NegExt e1 -> Expr e2) -> Neg e1 -> Expr e2
 desugNeg f (Neg e) = Mul (Const (-1)) (desug (desugNeg f) e)
 desugNeg f (NegExt e) = f e
 
